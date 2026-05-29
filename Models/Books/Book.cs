04e9b6efdac27520b82052df451dbe4398cb743d
Izzatjon Qodirov <qodirov.izzatjon.rambo.mb@gmail.com>
2026-05-29 08:34:44 +0500

    feat: Add Book model

---
+++ Models/Books/Book.cs
@@ -0,0 +1,10 @@
+namespace LibraryManagement.Models.Books;
+
+public class Book
+{
+	public int Id { get; set; }
+	public string Title { get; set; }
+	public string Author { get; set; }
+	public int Year { get; set; }
+	public bool IsAvailable { get; set; }
+}
